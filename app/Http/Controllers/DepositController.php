b6fa8136e81b564a888a860148afc72cf14a9248
Nathalia Hernandez <silvianathaliahernandezsandova@gmail.com>
2022-12-14 09:25:29 -0500

    controlador Deposit

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -7,58 +7,54 @@ use Illuminate\Http\Request;
 
 class DepositController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function create(Request $request)
+    {
+        try {
+            $validator = $request->validate([
+                'deposits' => 'required'
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
+        $user = User::create([
+            'deposits' => $request->deposits
+            
+        ]);
+
+        return response()->json($deposits,201);
+
+
+
+    }
+
     public function index()
     {
-        //
+        return Deposit::get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update($id, Request $request)
     {
-        //
+        Deposit::where('id', $id)
+            ->update(['deposits' => $request->deposits]);
+          
+
+
+        return 'Deposito actualizado con exito';
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Deposit  $deposit
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Deposit $deposit)
+    public function show($id)
     {
-        //
+        return Deposit::find($id);
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Deposit  $deposit
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Deposit $deposit)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Deposit  $deposit
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Deposit $deposit)
-    {
-        //
+        Deposit::where('id', $id)->delete();
+        return 'Deposito eliminado con exito ';
     }
 }
