3ea324adc657782b01e0331036aa8c19d6161e53
miguelacho01 <miguelcarvajal7.mc@gmail.com>
2022-12-14 07:39:43 -0500

    se crean migraciones

---
+++ app/Http/Controllers/AcountController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\acount;
+use Illuminate\Http\Request;
+
+class AcountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\acount  $acount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(acount $acount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\acount  $acount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, acount $acount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\acount  $acount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(acount $acount)
+    {
+        //
+    }
+}
